b903edcc9b6a0353dbd8724f4f1ed556b3246138
Ana Clara <ana05clara24@gmail.com>
2024-11-07 18:57:16 -0300

    fix: tornando os botões interativos com as sections

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,35 +1,54 @@
-import { AppBar, MenuItem, styled, Toolbar } from "@mui/material"
-import theme from "../../theme"
+import { AppBar, MenuItem, styled, Toolbar, Typography, Box } from "@mui/material";
+import theme from "../../theme";
+import logo from "../../../src/assets/png/logo.png";
 
+const NavBar = () => {
+  const StyledToolbar = styled(Toolbar)(() => ({
+    background: `linear-gradient(90deg, ${theme.palette.secondary.dark}, ${theme.palette.secondary.light})`,
+    display: "flex",
+    justifyContent: "space-between",
+  }));
 
-const NavBar = () =>{
+  const LogoContainer = styled(Box)(() => ({
+    display: "flex",
+    alignItems: "center",
+    cursor: "pointer",
+  }));
 
-    const StyledToobar = styled(Toolbar)(({}) =>({
-        display:"flex",
-        justifyContent: "space-evenly",
-    }))
+  const StyledMenu = styled(MenuItem)(() => ({
+    cursor: "pointer",
+    '&:hover': {
+      color: theme.palette.primary.contrastText,
+    },
+  }));
 
-    const StyledMenu = styled(MenuItem)(({}) =>({
-        cursor: "pointer",
-        '&:hover':{
-            color: theme.palette.primary.contrastText,
-        }
-    }))
+  const handleScroll = (sectionId: string) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
 
+  return (
+    <>
+      <AppBar position="fixed"> 
+        <StyledToolbar>
+          <LogoContainer onClick={() => handleScroll("hero")}> 
+            <img src={logo} alt="Logo Ana Clara" style={{ width: 60, height: 60 }} />
+            <Typography variant="h6" color="primary.contrastText" ml={1}>
+              Ana Clara
+            </Typography>
+          </LogoContainer>
 
-    return (
-      <>
-        <AppBar position="relative">
-            <StyledToobar>
-                <StyledMenu><MenuItem>About</MenuItem></StyledMenu>
-                <StyledMenu><MenuItem>Skills</MenuItem></StyledMenu>
-                <StyledMenu><MenuItem>Projects</MenuItem></StyledMenu>
-            </StyledToobar>
-        </AppBar>
-      </>
-    )
-  }
+          <Box display="flex" gap={2}>
+            <StyledMenu onClick={() => handleScroll("about")}>About</StyledMenu>
+            <StyledMenu onClick={() => handleScroll("skills")}>Skills</StyledMenu>
+            <StyledMenu onClick={() => handleScroll("projects")}>Projects</StyledMenu>
+          </Box>
+        </StyledToolbar>
+      </AppBar>
+    </>
+  );
+}
 
- 
 export default NavBar;
-  
