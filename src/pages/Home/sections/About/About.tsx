4ed16a5e40dd6822970fd70c1049d25bf08fa1b8
Ana Clara <ana05clara24@gmail.com>
2024-11-07 19:01:31 -0300

    feat: adicionando animação e melhorando o código

--- src/pages/Home/sections/About/About.tsx
+++ src/pages/Home/sections/About/About.tsx
@@ -1,60 +1,62 @@
 import { Container, Grid, Typography } from '@mui/material';
 import styled from '@mui/material/styles/styled';
 
-
 const StyledAboutSection = styled('section')(({ theme }) => ({
-    backgroundColor: theme.palette.secondary.light,
-    height: "105vh",
-    display: "flex",
-    alignItems: "center",
-    [theme.breakpoints.up('xs')]: { 
-    paddingTop: "100px",
-    },
-    [theme.breakpoints.up('md')]: { 
-        paddingTop: "0"
-    }
-  // ... other styles as needed
+  background: `linear-gradient(90deg, ${theme.palette.secondary.light}, ${theme.palette.secondary.dark})`,
+  minHeight: "100vh",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  padding: theme.spacing(4),
+}));
+
+const FadeInContainer = styled(Container)(({}) => ({
+  transition: "transform 0.7s ease",
+  opacity: 0,
+  transform: "scale(1)",
+  animation: `fadeIn 1s ease forwards 2s`,
+
+  '@keyframes fadeIn': {
+    from: { opacity: 0, transform: "translateY(20px)" },
+    to: { opacity: 1, transform: "translateY(0)" },
+  },
 }));
 
 const Home = () => {
   return (
-    <StyledAboutSection>
-      <Container maxWidth="lg">
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={8}>
-            <Typography variant="h3" textAlign="center" color="primary">
-            About Me
+    <StyledAboutSection id="about">
+      <FadeInContainer maxWidth="md">
+        <Grid container spacing={4} justifyContent="center" alignItems="center">
+          <Grid item xs={12}>
+            <Typography variant="h3" textAlign="center" color="primary.contrastText" gutterBottom>
+              About Me
             </Typography>
-            <Typography variant="h6" textAlign="center" color="textSecondary">
-                <br></br>Olá, sou estudante de Ciência da Computação pela Universidade Católica de Brasília. Minha jornada na programação começou em 2023, por pura curiosidade, ao me deparar com tutoriais de Python no YouTube. 
+            <Typography variant="h6" textAlign="center" color="textSecondary.dark" paragraph>
+              Olá, sou estudante de Ciência da Computação pela Universidade Católica de Brasília. Minha jornada na programação começou em 2023, por pura curiosidade, ao me deparar com tutoriais de Python no YouTube. 
             </Typography>
-            <Typography variant="h6" textAlign="center" color="textSecondary">
-                <br></br>Sempre fui apaixonada por idiomas e, por conta própria, alcancei o nível C1 em inglês. Na faculdade, descobri a Programação Orientada a Objetos e tenho me aprofundado nela.
+            <Typography variant="h6" textAlign="center" color="textSecondary.dark" paragraph>
+              Sempre fui apaixonada por idiomas e, por conta própria, alcancei o nível C1 em inglês. Na faculdade, descobri a Programação Orientada a Objetos e tenho me aprofundado nela.
             </Typography>
-            <Typography variant="h6" textAlign="center" color="textSecondary">
-                <br></br>Estou muito entusiasmada com a área de TI e os eventos que participo me proporcionam uma troca de experiências e conhecimentos valiosa.
+            <Typography variant="h6" textAlign="center" color="textSecondary.dark" paragraph>
+              Estou muito entusiasmada com a área de TI e os eventos que participo me proporcionam uma troca de experiências e conhecimentos valiosa.
             </Typography>
-            <Typography variant="h6" textAlign="center" color="textSecondary" >
-                <br></br>Acredito que o aprendizado contínuo e a busca por novos desafios são fundamentais para o meu crescimento profissional.
+            <Typography variant="h6" textAlign="center" color="textSecondary.dark" paragraph>
+              Acredito que o aprendizado contínuo e a busca por novos desafios são fundamentais para o meu crescimento profissional.
             </Typography>
-            <Typography variant="h5" textAlign="center" color="primary">
-            <br />- Curiosidades -
+            <Typography variant="h5" textAlign="center" color="primary.contrastText" gutterBottom>
+              - Curiosidades -
             </Typography>
-            <Typography variant="h6" textAlign="center" color="textSecondary">
-            <ul>
-                Minha comida favorita é pizza<br />
-                Meus hobbies preferidos é viajar, ler e estar com as pessoas que amo<br />
-                Sou super fã de fórmula 1, futebol e vôlei <br />
-                Também gosto de cozinhar, cantar e tocar alguns instrumentos
-            </ul>
+            <Typography variant="h6" textAlign="center" color="textSecondary.dark">
+              <ul style={{ listStyle: "none", padding: 0 }}>
+                <li>Minha comida favorita é pizza</li>
+                <li>Gosto de viajar, ler, cozinhar e estar com a família e os amigos</li>
+                <li>Sou bailarina e sou super fã de fórmula 1 e futebol</li>
+                <li>Já traduzi um livro infantil para o inglês e tenho poesias publicadas em coletâneas e antologias</li>
+              </ul>
             </Typography>
-          </Grid>
-          
-          <Grid item xs={12} md={4}>
-            
           </Grid>
         </Grid>
-      </Container>
+      </FadeInContainer>
     </StyledAboutSection>
   );
 };
