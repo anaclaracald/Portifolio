7f64618840b055292e2cd53a59a293c02f0b6f53
Ana Clara <ana05clara24@gmail.com>
2024-11-07 19:42:37 -0300

    feat: escrita animada no hero

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,33 +1,34 @@
-import styled from "@mui/material/styles/styled"
-import imagem from "../../../../assets/images/foto_perfil.jpg"
-import { Box, Container, Grid, Typography } from "@mui/material"
-import DownloadIcon from '@mui/icons-material/Download';
+import styled from "@mui/material/styles/styled";
+import imagem from "../../../../assets/images/foto_perfil.jpg";
+import { Box, Container, Grid, Typography } from "@mui/material";
+import DownloadIcon from "@mui/icons-material/Download";
 import StyledButton from "../../../../components/StyledButton/StyledButton";
 import { AnimatedBackground } from "../../../../components/AnimatedBackground/AnimatedBackground1";
-import cvfile from '../../../../assets/pdf/Resume.pdf';
+import cvfile from "../../../../assets/pdf/Resume.pdf";
+import Typewriter from "typewriter-effect"; 
 
 const Home = () => {
-  const StyledHero = styled("div") (({ theme }) => ({
+  const StyledHero = styled("div")(({ theme }) => ({
     backgroundColor: theme.palette.secondary.contrastText,
-    height:"103vh",
+    height: "103vh",
     display: "flex",
     alignItems: "center",
-    [theme.breakpoints.up('xs')]: { 
+    [theme.breakpoints.up("xs")]: {
       paddingTop: "100px",
     },
-    [theme.breakpoints.up('md')]: { 
-        paddingTop: "0"
-    }
-  }))
+    [theme.breakpoints.up("md")]: {
+      paddingTop: "0",
+    },
+  }));
 
-  const StyledImg = styled("img") (({theme}) => ({
+  const StyledImg = styled("img")(({ theme }) => ({
     width: "100%",
     borderRadius: "50%",
-    border: `1px solid ${theme.palette.secondary.dark}`
-  }))
+    border: `1px solid ${theme.palette.secondary.dark}`,
+  }));
 
   const handleDownloadCV = () => {
-    const link = document.createElement('a');
+    const link = document.createElement("a");
     link.href = cvfile;
     link.download = cvfile;
     link.click();
@@ -35,34 +36,44 @@ const Home = () => {
 
   return (
     <StyledHero>
-          <Container maxWidth="lg">
-            <Grid container>
-              <Grid item xs={12} md={4}>
-                <Box position="relative">
-                  <Box position="absolute" width={"150%"} top={100} right={0}>
-                      <AnimatedBackground />
-                  </Box>
-                  <Box position="relative" textAlign="center">
-                      <StyledImg src={imagem} />
-                  </Box>
-                </Box>            
-              </Grid>
-              <Grid item xs={12} md={8}>
-                <Typography color="secondary" variant="h1" textAlign="center" pb={2}>Ana Clara</Typography>
-                <Typography color="secondary" variant="h2" textAlign="center">I'm a software engineer!</Typography>
-                <Grid container display="flex" justifyContent="center" spacing={3} pt={3}>
-                  <Grid item xs={12} md={4} display="flex" justifyContent="center">
-                    <StyledButton onClick={handleDownloadCV} startIcon={<DownloadIcon />}>
-                      Download CV</StyledButton></Grid>
-                </Grid>  
+      <Container maxWidth="lg">
+        <Grid container>
+          <Grid item xs={12} md={4}>
+            <Box position="relative">
+              <Box position="absolute" width={"150%"} top={100} right={0}>
+                <AnimatedBackground />
+              </Box>
+              <Box position="relative" textAlign="center">
+                <StyledImg src={imagem} />
+              </Box>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={8}>
+            <Typography color="secondary" variant="h1" textAlign="center" pb={2}>
+              Ana Clara
+            </Typography>
+            <Typography color="secondary" variant="h2" textAlign="center">
+              <Typewriter
+                options={{
+                  strings: ["I'm a software engineer!"],
+                  autoStart: true,
+                  loop: true,
+                  delay: 200,
+                }}
+              />
+            </Typography>
+            <Grid container display="flex" justifyContent="center" spacing={3} pt={3}>
+              <Grid item xs={12} md={4} display="flex" justifyContent="center">
+                <StyledButton onClick={handleDownloadCV} startIcon={<DownloadIcon />}>
+                  Download CV
+                </StyledButton>
               </Grid>
             </Grid>
-          </Container>
-        
-        </StyledHero>
-  )
+          </Grid>
+        </Grid>
+      </Container>
+    </StyledHero>
+  );
+};
 
-}
-  
-export default Home
-
+export default Home;
