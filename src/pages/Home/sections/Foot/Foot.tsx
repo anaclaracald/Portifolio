6502d066fe27896e9782f37c72717fc99404e805
Ana Clara <ana05clara24@gmail.com>
2024-11-07 19:20:32 -0300

    fix: icone não utilizado

--- src/pages/Home/sections/Foot/Foot.tsx
+++ src/pages/Home/sections/Foot/Foot.tsx
@@ -3,7 +3,6 @@ import { Container, Typography, IconButton } from "@mui/material";
 import CopyrightIcon from '@mui/icons-material/Copyright';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import EmailIcon from '@mui/icons-material/Email';
 
 const Home = () => {
 
